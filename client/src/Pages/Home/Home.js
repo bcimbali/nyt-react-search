9f2ae8dabcdd14728ffccbaee2f1328896b05f65
Brett Cimbalik <bcimbali@gmail.com>
2018-07-01 11:12:23 -0500

    fleshed out file structure

---
+++ client/src/Pages/Home/Home.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import API from './../../utils/API.js';
+import Article from './../../Components/Article';
+
+class Home extends Component {
+    state = {
+        articles: [],
+        savedArticles: [],
+        q: "",
+        start_year: "",
+        end_year: ""
+    }
+
+    componentDidMount() {
+        this.getArticles();
+        this.getSavedArticles();
+    }
+
+    // my methods here
+    getArticles = () => {
+        API.getArticles({
+            q: this.state.q,
+            start_year: this.state.start_year,
+            end_year: this.state.end_year
+        })
+        .then(res => 
+        this.setState({
+            articles: res.data,
+        })
+      )
+      .catch(err => console.log(err));
+    }
+
+    handleArticleSave = id => {
+        const article = this.state.articles.find(article => article._id === id);
+        API.saveArticle(article).then(res => {this.getSavedArticles();this.getArticles()});
+    }
+    getSavedArticles = () => {
+        API.getSavedArticles()
+            .then(res =>
+            this.setState({
+                savedArticles: res.data
+            })
+        )
+        .catch(err => console.log(err));
+    }
+
+    // Render gets called when state changes
+    render() {
+        return (
+            <div>
+                <p>I am a list of articles.</p>
+                {this.state.articles.map(article => (
+                    <Article
+                        key={article._id}
+                        _id={article._id}
+                        title={article.headline.main}
+                        url={article.web_url}
+                        date={article.pub_date}
+                        handleClick={this.handleArticleSave}
+                        buttonText="Save Article"
+                    />
+                ))}
+                <p>Saved Articles</p>
+                {this.state.savedArticles.map(article => (
+                    <Article
+                        key={article._id}
+                        _id={article._id}
+                        title={article.title}                  
+                        buttonText="Delete Article"
+                    />
+                ))}
+            </div>
+        )
+    }
+};
+
+export default Home;
---
+++ client/src/utils/filterParams.js
@@ -0,0 +1,11 @@
+//   takes in params obj and returns obj with null keys removed
+
+
+// Takes in a params object and returns the object with null/empty keys removed
+export default params =>
+  Object.keys(params)
+    .filter(key => params[key])
+    .reduce((acc, curr) => {
+      acc[curr] = params[curr];
+      return acc;
+    }, {});
